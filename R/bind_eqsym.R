e859fd26fee3379a6b97ad2de28c4c6ba495690a
Vince Carey <stvjc@channing.harvard.edu>
2021-01-24 08:58:41 -0500

    moving forward with phewas/eqtl lookup

---
+++ R/bind_eqsym.R
@@ -0,0 +1,20 @@
+
+#' helper for converting ENSG to symbols, cbinding symbol and chromosome name for the genes to the input data.frame-like entity
+#' @param dflike a data.frame-like entity with a column of ENSG gene identifiers; column
+#' name must be the value of `bind_sym` parameter `ensgvbl`
+#' @param gene_resource a data.frame with columns `gene_name` and `seqnames`,
+#' defaulting to `gwaslake::genes_v75`
+#' @param ensgvbl character(1) the column of `dflike` holding the ENSG ids
+#' @param output_symvbl character(1) name to be used for symbol in output data.frame, defaults to `symbol`
+#' @param output_chromvbl character(1) name to be used for chromosome in output data.frame, defaults to `gchrom`
+#' @examples
+#' head(bind_sym(little_eq))
+#' @export
+bind_sym = function( dflike, gene_resource = genes_v75, ensgvbl = "trait",
+   output_symvbl = "symbol", output_chromvbl = "gchrom" ) {
+ stopifnot(ensgvbl %in% names(dflike))
+ stopifnot(all(c("gene_name", "seqnames") %in% names(gene_resource)))
+ dflike[[output_symvbl]] = gene_resource[ dflike[[ ensgvbl ]], "gene_name" ] 
+ dflike[[output_chromvbl]] = as.character(factor(gene_resource[ dflike[[ ensgvbl ]], "seqnames" ] ))
+ dflike
+}
---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' a data.frame of genes(EnsDb.Hsapiens.v75) with build appended
+#' @docType data
+#' @format data.frame
+#' @examples
+#' head(genes_v75,3)
+"genes_v75"
+
+#' a data.frame of eQTL results for testing
+#' @docType data
+#' @format data.frame
+#' @examples
+#' head(little_eq)
+"little_eq"
